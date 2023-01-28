c244ca9c1ce12ac214410bfb5c2f32bf42428a8c
Xavier Coiffic <xavier@woho.io>
2023-01-29 02:10:59 +0400

    frontend updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,19 +9,54 @@
 Product.destroy_all
 
 Product.create!([{
+  user_id: 1,
   title: "Lightsaber",
-  price: "400",
-  description: "A fully functional lightsaber that was taken from a jedi dead body."
+  price: "40000",
+  description: "A fully functional lightsaber that was taken from a jedi dead body.",
+  photo: "https://wohomedia.b-cdn.net/lewagon/jabbasouk/lightsaber.png"
 },
 {
+  user_id: 1,
   title: "Boring Droid",
   price: "5600",
-  description: "A useless droid, which will looks great in your appartment."
+  description: "A useless droid, which will looks great in your appartment.",
+  photo: "https://wohomedia.b-cdn.net/lewagon/jabbasouk/c3po.png"
 },
 {
-  title: "Ewok leg",
-  price: "200",
-  description: "An Ewok leg which will taste perfectly with some marinated space BBQ sauce."
+  user_id: 1,
+  title: "BB-8",
+  price: "5600",
+  description: "In very good condition. This droid is the perfect companion.",
+  photo: "https://wohomedia.b-cdn.net/lewagon/jabbasouk/bb8.png"
+},
+{
+  user_id: 1,
+  title: "Trooper Helmet",
+  price: "2000",
+  description: "Riding a space bike? Get cooler with this helmet.",
+  photo: "https://wohomedia.b-cdn.net/lewagon/jabbasouk/stormtrooper.png"
+},
+{
+  user_id: 1,
+  title: "Death Star",
+  price: "5000",
+  description: "okay, it's not the real one, but it's still a cool toy.",
+  photo: "https://wohomedia.b-cdn.net/lewagon/jabbasouk/deathstar.png"
+},
+{
+  user_id: 1,
+  title: "Crunchie Ewok",
+  price: "600",
+  description: "In a soup or roasted, this Ewok will be delicious.",
+  photo: "https://wohomedia.b-cdn.net/lewagon/jabbasouk/funnyewok.png"
+},
+{
+  user_id: 1,
+  title: "Baby Yoda",
+  price: "600",
+  description: "The cutest thing in the galaxy. You will love it.",
+  photo: "https://wohomedia.b-cdn.net/lewagon/jabbasouk/babyyoda.png"
+  
 }])
 
 p "Created #{Product.count} products"
