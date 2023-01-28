a0760863faf2b91b43f8af3fccb0ca20298c1d38
neildus <dussoye.neil@gmail.com>
2023-01-28 15:03:56 +0400

    added resources for users, products and bookings

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get '/products' => 'pages#product'
+  resources :users
+  resources :products
+  resources :bookings
 end
