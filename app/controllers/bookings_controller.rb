5d4f8de666e927358736f6406462f97a66910a0b
neildus <dussoye.neil@gmail.com>
2023-01-28 15:52:17 +0400

    Bookings controller done

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,39 @@
+class BookingsController < ApplicationController
+  before_action :set_booking, only: [:show, :destroy]
+
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @booking.destroy
+    redirect_to bookings_path, status: :see_other
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:product_id, :user_id)
+  end
+end
