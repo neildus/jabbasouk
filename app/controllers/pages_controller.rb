ca78303cbbcc7473d9a6605be9f7bb2619f73003
Azhar Ollibokus <azhar4@gmail.com>
2023-01-28 13:28:13 +0400

    added product page to route

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, except: [:product, :index]
+
   def home
   end
 end
